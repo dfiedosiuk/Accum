e3fe32781a8edf3331627b92df38a4acf2c3f4f6
Daniel <d.fiedosiuk@adform.com>
2022-03-31 23:49:32 +0200

    init commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,7 @@
+object Main extends App {
+
+//  accum("abcd") -> "A-Bb-Ccc-Dddd"
+  def accum(s: String): String ={
+    ???
+  }
+}
---
+++ src/test/scala/MainSpec.scala
@@ -0,0 +1,15 @@
+import Main.accum
+import org.scalatest._
+import flatspec._
+import matchers._
+
+
+class alphabetPositionSpec extends AnyFlatSpec with should.Matchers {
+
+  behavior of "An accum"
+
+  it should "return string with specific pattern " in {
+    accum("abcd") shouldBe "A-Bb-Ccc-Dddd"
+  }
+
+}
